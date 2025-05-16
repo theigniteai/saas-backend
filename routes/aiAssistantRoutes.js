dcd1360ba127370de21adbcc619bea398dd72e4a
theigniteai <theignite.ai@gmail.com>
2025-05-16 20:33:38 +0400

    Update aiAssistantRoutes.js

--- routes/aiAssistantRoutes.js
+++ routes/aiAssistantRoutes.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { generateAIResponse } from '../controllers/aiAssistantController.js'
 
 const router = express.Router()
+
 router.post('/respond', generateAIResponse)
 
 export default router
