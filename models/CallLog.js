89f761b7e61bb095d95076a637b2e49f7496e841
theigniteai <theignite.ai@gmail.com>
2025-05-24 16:50:04 +0400

    Update CallLog.js

--- models/CallLog.js
+++ models/CallLog.js
@@ -1,21 +1,13 @@
 // models/CallLog.js
 import mongoose from "mongoose";
 
-const CallLogSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "User",
-    },
-    from: String,
-    to: String,
-    userSpeech: String,
-    aiReply: String,
-    recordingUrl: String,
-  },
-  { timestamps: true }
-);
+const CallLogSchema = new mongoose.Schema({
+  userId: { type: mongoose.Types.ObjectId, required: true },
+  from: { type: String, required: true },
+  to: { type: String, required: true },
+  userSpeech: { type: String },
+  aiReply: { type: String },
+  recordingUrl: { type: String },
+}, { timestamps: true });
 
-const CallLog = mongoose.model("CallLog", CallLogSchema);
-export default CallLog;
+export default mongoose.model("CallLog", CallLogSchema, "calllogs");
